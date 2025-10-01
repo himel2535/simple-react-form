d9f1ea29d6a95bc17aaa2505742a047655929c3c
himel2535 <monwarhossanhimel@gmail.com>
2025-10-01 23:43:49 +0600

    uncontrolled ways to collect form data

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import ControlledField from "./components/ControlledField/ControlledField";
 import FormAction from "./components/FormAction/FormAction";
 import SimpleForm from "./components/SimpleForm/SimpleForm";
+import UncontrolledField from "./components/UncontrolledField/UncontrolledField";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <h1>explore react form</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <FormAction></FormAction> */}
-      <ControlledField></ControlledField>
+      {/* <ControlledField></ControlledField> */}
+      <UncontrolledField></UncontrolledField>
     </>
   );
 }
